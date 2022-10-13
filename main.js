c59654f8cf84d353a9e01c672c2d301faf223038
pedromoussa <pedropaulo.moussa@gmail.com>
2022-10-13 15:46:24 -0300

    add html css and js

---
+++ main.js
@@ -0,0 +1,44 @@
+let numCircles = 0;
+let className = ['red-circle', 'green-circle', 'blue-circle'];
+
+window.onload = function () {
+
+    // cria 3 primeiros circulos
+    while(numCircles < 3) {
+        addCircle();
+    }
+
+    // cria botoes de adicionar e remover circulos
+    const add = document.createElement('button');
+    const sub = document.createElement('button');
+
+    add.className = 'add-button';
+    sub.className = 'sub-button';
+
+    add.addEventListener('click', addCircle);
+    sub.addEventListener('click', removeCircle);
+
+    document.body.appendChild(add);
+    document.body.appendChild(sub);
+
+}
+
+//funcao de criar os circulos
+function addCircle() {
+
+    const circle = document.createElement('div');
+    circle.className = className[numCircles%3];
+    document.body.appendChild(circle);
+
+    numCircles++;
+
+}
+
+//funcao de remover circulos
+function removeCircle() {
+
+    numCircles--;
+
+}
+
+// TODO: Implementar removeCircle() e visibilidade dos circulos
